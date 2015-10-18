38c0fb9af2173fbdf4bc2e738ce46ce329cc42b7
Cesar Ilharco Magalhaes <cesarim22@gmail.com>
2015-10-18 23:49:08 +0200

    Initializing buttons in Activity

--- app/src/main/java/com/myapp/calculator/CalculatorActivity.java
+++ app/src/main/java/com/myapp/calculator/CalculatorActivity.java
@@ -1,5 +1,6 @@
 package com.myapp.calculator;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -17,6 +18,7 @@ public class CalculatorActivity extends AppCompatActivity implements OnClickList
     private TextView expression;
     private TextView result;
 
+    @SuppressLint("NewApi")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
@@ -26,12 +28,8 @@ public class CalculatorActivity extends AppCompatActivity implements OnClickList
         result = (TextView) findViewById(R.id.resultView);
 
         // Set buttons, handle land-view only cases.
-        findViewById(R.id.button0).setOnClickListener(this);
-
-        if (findViewById(R.id.buttonSqrt) != null) {
-            findViewById(R.id.buttonSqrt).setOnClickListener(this);
-        }
-
+        initializeButtons();
+        initializeLandButtons();
     }
 
     @Override
@@ -56,4 +54,56 @@ public class CalculatorActivity extends AppCompatActivity implements OnClickList
         super.onRestoreInstanceState(savedInstanceState);
     }
 
+    private void initializeButtons(){
+        findViewById(R.id.button0).setOnClickListener(this);
+        findViewById(R.id.button1).setOnClickListener(this);
+        findViewById(R.id.button2).setOnClickListener(this);
+        findViewById(R.id.button3).setOnClickListener(this);
+        findViewById(R.id.button4).setOnClickListener(this);
+        findViewById(R.id.button5).setOnClickListener(this);
+        findViewById(R.id.button6).setOnClickListener(this);
+        findViewById(R.id.button7).setOnClickListener(this);
+        findViewById(R.id.button8).setOnClickListener(this);
+        findViewById(R.id.button9).setOnClickListener(this);
+        findViewById(R.id.button00).setOnClickListener(this);
+        findViewById(R.id.buttonDot).setOnClickListener(this);
+        findViewById(R.id.buttonEquals).setOnClickListener(this);
+        findViewById(R.id.buttonAdd).setOnClickListener(this);
+        findViewById(R.id.buttonSubtract).setOnClickListener(this);
+        findViewById(R.id.buttonMultiply).setOnClickListener(this);
+        findViewById(R.id.buttonDivide).setOnClickListener(this);
+    }
+
+    private void initializeLandButtons(){
+        if (findViewById(R.id.buttonOpenP) != null) {
+            findViewById(R.id.buttonOpenP).setOnClickListener(this);
+        }
+        if (findViewById(R.id.buttonCloseP) != null) {
+            findViewById(R.id.buttonCloseP).setOnClickListener(this);
+        }
+        if (findViewById(R.id.buttonSqrt) != null) {
+            findViewById(R.id.buttonSqrt).setOnClickListener(this);
+        }
+        if (findViewById(R.id.buttonSin) != null) {
+            findViewById(R.id.buttonSin).setOnClickListener(this);
+        }
+        if (findViewById(R.id.buttonCos) != null) {
+            findViewById(R.id.buttonCos).setOnClickListener(this);
+        }
+        if (findViewById(R.id.buttonTan) != null) {
+            findViewById(R.id.buttonTan).setOnClickListener(this);
+        }
+        if (findViewById(R.id.buttonFact) != null) {
+            findViewById(R.id.buttonFact).setOnClickListener(this);
+        }
+        if (findViewById(R.id.buttonFib) != null) {
+            findViewById(R.id.buttonFib).setOnClickListener(this);
+        }
+        if (findViewById(R.id.buttonLog) != null) {
+            findViewById(R.id.buttonLog).setOnClickListener(this);
+        }
+        if (findViewById(R.id.buttonExp) != null) {
+            findViewById(R.id.buttonExp).setOnClickListener(this);
+        }
+    }
 }
