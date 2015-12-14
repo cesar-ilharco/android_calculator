4221ce226c691b6af0b028e7517618e4b2407645
Cesar Ilharco Magalhaes <cesar.ilharco@gmail.com>
2015-12-14 21:11:20 -0200

    created Kernel.evaluate unittest

--- app/src/test/java/com/myapp/calculator/KernelTests.java
+++ app/src/test/java/com/myapp/calculator/KernelTests.java
@@ -12,6 +12,7 @@ import junit.framework.Assert;
 import org.junit.Test;
 
 import java.io.IOException;
+import java.math.BigDecimal;
 import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.List;
@@ -116,6 +117,80 @@ public class KernelTests {
         expression.getUnits().addLast(new OperatorUnit("-"));
         expression.getUnits().addLast(new DigitUnit("2"));
         Assert.assertEquals("2", Kernel.evaluateBasic(expression.getUnits(), 0));
+    }
+
+    @Test
+    public void testKernelEvaluator() throws IOException{
+        Expression expression = new Expression();
+        // 1 + 2 = 3
+        expression.getUnits().addLast(new DigitUnit("1"));
+        expression.getUnits().addLast(new OperatorUnit("+"));
+        expression.getUnits().addLast(new DigitUnit("2"));
+        Assert.assertEquals(new BigDecimal("3"), Kernel.evaluate(expression, 0, AngleUnit.DEGREE));
+        Assert.assertEquals(new BigDecimal("3"), Kernel.evaluate(expression, 1000, AngleUnit.DEGREE));
+
+        // 1 + 2 × 4 - 3 = 6
+        expression.getUnits().addLast(new OperatorUnit("×"));
+        expression.getUnits().addLast(new DigitUnit("4"));
+        expression.getUnits().addLast(new OperatorUnit("-"));
+        expression.getUnits().addLast(new DigitUnit("3"));
+        Assert.assertEquals(new BigDecimal("6"), Kernel.evaluate(expression, 0, AngleUnit.RADIANS));
+        Assert.assertEquals(new BigDecimal("6"), Kernel.evaluate(expression, 1000, AngleUnit.DEGREE));
+
+        // 1 × 2 × 3 × 4 × 5 = 120
+        expression.getUnits().clear();
+        expression.getUnits().addLast(new DigitUnit("1"));
+        expression.getUnits().addLast(new OperatorUnit("×"));
+        expression.getUnits().addLast(new DigitUnit("2"));
+        expression.getUnits().addLast(new OperatorUnit("×"));
+        expression.getUnits().addLast(new DigitUnit("3"));
+        expression.getUnits().addLast(new OperatorUnit("×"));
+        expression.getUnits().addLast(new DigitUnit("4"));
+        expression.getUnits().addLast(new OperatorUnit("×"));
+        expression.getUnits().addLast(new DigitUnit("5"));
+        Assert.assertEquals(new BigDecimal("120"), Kernel.evaluate(expression, 0, AngleUnit.DEGREE));
+        Assert.assertEquals(new BigDecimal("120"), Kernel.evaluate(expression, 1000, AngleUnit.DEGREE));
+
+        // 1 - 2 × 3 + 4 × 5 = 15
+        expression.getUnits().clear();
+        expression.getUnits().addLast(new DigitUnit("1"));
+        expression.getUnits().addLast(new OperatorUnit("-"));
+        expression.getUnits().addLast(new DigitUnit("2"));
+        expression.getUnits().addLast(new OperatorUnit("×"));
+        expression.getUnits().addLast(new DigitUnit("3"));
+        expression.getUnits().addLast(new OperatorUnit("+"));
+        expression.getUnits().addLast(new DigitUnit("4"));
+        expression.getUnits().addLast(new OperatorUnit("×"));
+        expression.getUnits().addLast(new DigitUnit("5"));
+        Assert.assertEquals(new BigDecimal("15"), Kernel.evaluate(expression, 0, AngleUnit.DEGREE));
+        Assert.assertEquals(new BigDecimal("15"), Kernel.evaluate(expression, 1000, AngleUnit.DEGREE));
+
+        // 1 / 3 = 0.333...
+        expression.getUnits().clear();
+        expression.getUnits().addLast(new DigitUnit("1"));
+        expression.getUnits().addLast(new OperatorUnit("/"));
+        expression.getUnits().addLast(new DigitUnit("3"));
+        Assert.assertEquals(new BigDecimal("0"), Kernel.evaluate(expression, 0, AngleUnit.DEGREE));
+        Assert.assertEquals(new BigDecimal("0.3"), Kernel.evaluate(expression, 1, AngleUnit.DEGREE));
+        Assert.assertEquals(new BigDecimal("0.3333333333"), Kernel.evaluate(expression, 10, AngleUnit.DEGREE));
+        StringBuilder oneThird = new StringBuilder("0.");
+        int scale = 2000;
+        for (int i=0; i<scale; i++){
+            oneThird.append("3");
+        }
+        Assert.assertEquals(new BigDecimal(oneThird.toString()), Kernel.evaluate(expression, scale, AngleUnit.DEGREE));
+
+        // -1 × -2 = 2
+        expression.getUnits().clear();
+        expression.getUnits().addLast(new OperatorUnit("-"));
+        expression.getUnits().addLast(new DigitUnit("1"));
+        expression.getUnits().addLast(new OperatorUnit("×"));
+        expression.getUnits().addLast(new OperatorUnit("-"));
+        expression.getUnits().addLast(new DigitUnit("2"));
+        Assert.assertEquals(new BigDecimal("2"), Kernel.evaluate(expression, 0, AngleUnit.DEGREE));
+        Assert.assertEquals(new BigDecimal("2"), Kernel.evaluate(expression, 1000, AngleUnit.DEGREE));
+
+        // TODO: Add parsing for more complex expressions:
 
     }
 
